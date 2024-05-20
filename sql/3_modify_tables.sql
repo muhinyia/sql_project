f084d19275f3a6ebcebc67d435fd37930f0a5081
muhinyia <muhinyiandegwa@gmail.com>
2024-05-20 15:41:02 +0100

    removed data files due to size restrictions

--- sql/3_modify_tables.sql
+++ sql/3_modify_tables.sql
@@ -23,20 +23,19 @@ NOTE: If you are having issues with permissions. And you get error:
 
 */
 
--- NOTE: This has been updated from the video to fix issues with encoding
 
 \COPY company_dim
-FROM '/Users/muhinyia/Projects/SQL/PostgreSQL/all_folders/csv_files/company_dim.csv'
+FROM '/Users/muhinyia/Projects/SQL/PostgreSQL/sql_project/data/csv_files/company_dim.csv'
 WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
 \COPY skills_dim
-FROM '/Users/muhinyia/Projects/SQL/PostgreSQL/all_folders/csv_files/skills_dim.csv'
+FROM '/Users/muhinyia/Projects/SQL/PostgreSQL/sql_project/data/csv_files/skills_dim.csv'
 WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
 \COPY job_postings_fact
-FROM '/Users/muhinyia/Projects/SQL/PostgreSQL/all_folders/csv_files/job_postings_fact.csv'
+FROM '/Users/muhinyia/Projects/SQL/PostgreSQL/sql_project/data/csv_files/job_postings_fact.csv'
 WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
 \COPY skills_job_dim
-FROM '/Users/muhinyia/Projects/SQL/PostgreSQL/all_folders/csv_files/skills_job_dim.csv'
+FROM '/Users/muhinyia/Projects/SQL/PostgreSQL/sql_project/datacsv_files/skills_job_dim.csv'
 WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
